29d5e40dbd5c9e7cf36ae2da25784536ab97fc5f
Roukaiya-sketch <71189419+Roukaiya-sketch@users.noreply.github.com>
2023-05-19 00:04:48 +0400

    Add files via upload

---
+++ Task2/lib/screen2.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class screen2 extends StatelessWidget {
+  const screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
